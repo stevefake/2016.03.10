6d8f50244282ab8d05898e4a2f8a087c334ca85c
Steve Fake <steven.m.fake@gmail.com>
2016-03-15 15:13:52 -0400

    add add, subtct, & multply functions

--- app.rb
+++ app.rb
@@ -15,16 +15,75 @@ require_relative 'currency.rb'
 
 USD_currency = Currency.new(12) #remember to caps the method
 
-  USD_currency.set_amount_of_currency(12) # => 12
+USD_currency.set_amount_of_currency(12) # => 12
+USD_currency
+
+
+# amount_of_currency
 #  JPY_currency.set_amount_of_currency(16)    # => 16
 
 # Should be created with an amount and a currency code
 
-# puts amount
-# puts Currency_dollar = Currency_dollar.new(1)
-# puts currency_yen
-# puts currency_yen
+dollar = Currency.new(1)
+dollar.currency_dollar
+
+yen = Currency.new(1)
+yen.currency_yen
+
+euro = Currency.new(1)
+euro.currency_euro
+
+#############
+# Should equal another Currency object with the same amount and currency code
+# Should NOT equal another Currency object with different amount or currency code
+
+puts "Currency object equals another currency object with the same amount and currency code?"
+
+if dollar.currency_dollar == dollar.currency_dollar
+  print "#{dollar.currency_dollar} == #{dollar.currency_dollar}"
+  puts true
+else
+  puts false
+end
+
+puts "Currency object does NOT equal another currency object with different amount or currency code"
+if yen.currency_yen != euro.currency_euro
+  puts true
+else
+  puts false
+end
+  #
+
 
 ######
 trex = Currency.new(1)
 trex.speak
+
+trex = Dinosaur.new
+trex.speak("RAWRRRR", "T-Rex")
+
+###
+# THOUGHTS ON HW:
+#
+# prolly use a local variable as the arbitrary currency value to set
+# and…
+# …use instance variable @sign as the fixed conversion?
+#
+#
+# using both of these allows you to get different results for two class instances from one defined method
+#
+# def set_amount_of_currency(new_amount)
+#   @amoung_of_currency = new_amount
+# end
+# USD_currency.set_amount_of_currency(12) # => 12
+# JPY_currency.set_amount_of_currency(16)    # => 16
+#
+# def add_currency
+#   size_of_add = 2.6
+#   @amount_of_currency -= size_of_add
+# end
+#
+# def add_currency(size_of_add = 2.6)   #default argument
+#   @amount_of_currency -= size_of_add
+# end
+#
--- currency.rb
+++ currency.rb
@@ -1,19 +1,3 @@
-#Create A Currency class in its own file.
-
-# represent an amount of currency (a real-world thing you can point to)
-
-# Currency objects:
-#
-# Should be created with an amount and a currency code
-# Should equal another Currency object with the same amount and currency code
-# Should NOT equal another Currency object with different amount or currency code
-# Should be able to be added to another Currency object with the same currency code
-# Should be able to be subtracted by another Currency object with the same currency code
-# Should raise a DifferentCurrencyCodeError when you try to add or subtract two Currency objects with different currency codes.
-# Should be able to be multiplied by a Fixnum or Float and return a Currency object
-# Currency.new should be able to take one argument with a currency symbol
-#  embedded in it, like "$1.20" or "€ 7.00", and figure out the correct currency
-#  code. It can also take two arguments like before, one being the amount and the other being the currency code.
 
 #line7: currency_code = USD
 #       currency_amount = 10
@@ -44,8 +28,74 @@
 # Currency objects:
 # Should be created with an amount and a currency code
 
-
 class Currency
+  # attr_reader: :amount, :code
+
+  def initialize(amount, code)  # ~> ArgumentError: wrong number of arguments (given 1, expected 2)
+     @amount = amount           # => 23, 2
+     @code = code               # => :USD, :USD
+   end                          # => :initialize
+
+  def amount
+    @amount * 1.2  # => 27.599999999999998, 2.4, 2.4, 2.4, 2.4, 2.4
+  end              # => :amount
+
+  def code
+    @code   # => :USD, :USD
+  end       # => :code
+
+  def add_simple
+    @amount + 5   # => 28
+  end             # => :add_simple
+
+  def +(other)
+    new_amount = @amount + other.amount  # => 25.4, 25.4, 25.4
+  end                                    # => :+
+
+  def add(other)
+    self + other  # => 25.4
+  end             # => :add
+
+  def -(other)
+    new_amount = @amount - other.amount  # => 20.6
+  end                                    # => :-
+
+  def *(other)
+    new_amount = @amount * other  # => 184, 16.0
+  end                             # => :*
+
+end  # => :*
+
+c1 = Currency.new(23, :USD)  # => #<Currency:0x007fd944188218 @amount=23, @code=:USD>
+c2 = Currency.new(2, :USD)   # => #<Currency:0x007fd944189078 @amount=2, @code=:USD>
+
+c1.amount  # => 27.599999999999998
+c1.code    # => :USD
+c2.amount  # => 2.4
+c2.code    # => :USD
+
+c1 + c2   # => 25.4
+c1.+(c2)  # => 25.4
+
+c1.add_simple  # => 28
+
+c1.add(c2)  # => 25.4
+# c1.add(6)
+
+c1 - c2  # => 20.6
+
+c1 * 8    # => 184
+c2 * 8.0  # => 16.0
+
+c1 = Currency.new("$23.00")
+c2 = Currency.new("$2.00")
+
+
+# amount = 1
+# code = USD
+# puts @amount
+# puts @code
+
 #   def initialize(string) #to strip the currency symbol
 #     split_money = string.split("")
 #     currency_type = split_money.shift #takes off the first value of the array (the "$")
@@ -72,46 +122,107 @@ class Currency
 # c2.amount
 
 
-  def initialize(amount)
-    @amount
-  end
 
-  def value
-    @amount
-  end
-
-  def set_amount_of_currency(new_amount)
-    @amoung_of_currency = new_amount
-  end
+  # def value
+  #   @amount
+  # end
+  #
+  # def set_amount_of_currency(new_amount)
+  #   @amount_of_currency = new_amount
+  # end
 
   # USD_currency.set_amount_of_currency(12) # => 12
   # JPY_currency.set_amount_of_currency(16)    # => 16
 
-  def add_currency
-    size_of_add = 2.6
-    @amount_of_currency -= size_of_add
-  end
-
-  def add_currency(size_of_add = 2.6)   #default argument
-    @amount_of_currency -= size_of_add
-  end
+  # def add_currency
+  #   size_of_add = 2.6
+  #   @amount_of_currency -= size_of_add
+  # end
+  #
+  # def add_currency(size_of_add = 2.6)   #default argument
+  #   @amount_of_currency -= size_of_add
+  # end
 
 # Currency objects:
 # Should be created with an amount and a currency code
+# def currency_dollar
+#   puts dollar = "$1"
+# end
+#
+# def currency_yen
+#   puts yen = "¥1"
+# end
+#
+# def currency_euro
+#   puts euro = "€1"
+# end
+######
 
-def currency_dollar
-  @currency_dollar = $1
-end
-
-def currency_yen
-  currency_yen = ¥1
-end
-def currency_euro
-  currency_euro = €1
-end
+# dollar.currency_dollar(1) = euro.currency_euro(0.9)
+# = yen.currency_yen(113.8)
 
 ######
-def speak
-    puts "RAWRRRR"
-  end
-end
+
+
+# def currency_code(code)
+#   @code = code
+# end
+#
+# def initialize(code) # `initialize': wrong number of arguments
+#                      # (given 0, expected 1) (ArgumentError)
+#     @code = code
+# end
+#
+# def set_code(code)
+#     @code = code
+# end
+#
+# dollar = Currency.new
+#
+# dollar.set_code("USD") # `<top (required)>': undefined method `set_code'
+#                          # for #<Currency:0x007ffb13058ac8> (NoMethodError)
+#                        # undefined local variable or method `dollar' for
+#                        # main:Object (NameError)
+#
+# def code
+#    @code
+#  end
+#
+# dollar = Currency.new("USD") # `initialize': wrong number of arguments
+#                              # (given 1, expected 0) (ArgumentError)
+
+
+######
+
+# def speak
+#     puts "RAWRRRR"
+# end
+#
+#
+# #class Dinosaur
+#   def speak(roar, type)
+#     puts "The #{type} roared, \"#{roar}\""
+#   end
+#
+#
+# ###
+# #class Dinosaur # Currency
+#   def set_type(type) # currency_code
+#     @type = type # @code
+#   end
+#
+#
+# trex = Dinosaur.new # dollar = Currency.new
+# #=> #<Dinosaur:0x007fd90387c510>
+#
+# trex.set_type("T-Rex") # dollar.set_code(USD)
+# # => "T-Rex"
+#
+# trex #USD
+
+# ~> ArgumentError
+# ~> wrong number of arguments (given 1, expected 2)
+# ~>
+# ~> /Users/stevefake/Desktop/iron_yard/2016.03.10_git_repo/currency.rb:34:in `initialize'
+# ~> /Users/stevefake/Desktop/iron_yard/2016.03.10_git_repo/currency.rb:90:in `new'
+# ~> /Users/stevefake/Desktop/iron_yard/2016.03.10_git_repo/currency.rb:90:in `<main>'
